9daf0a82e359a33f015c6003b668e4fd014b04dd
csnand <askmryang@outlook.com>
2018-11-06 17:04:53 +0000

    add listview

--- app/src/main/java/com/g53mdp/mp3player/MainActivity.java
+++ app/src/main/java/com/g53mdp/mp3player/MainActivity.java
@@ -1,7 +1,15 @@
 package com.g53mdp.mp3player;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.io.File;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+
+    private void init(){
+
+        ListView dirList = (ListView) findViewById(R.id.dirListView);
+
+        File musicDir = new File(Environment.getExternalStorageDirectory().getPath() + "/Music/");
+
+        File list[] = musicDir.listFiles();
+
+        dirList.setAdapter(new ArrayAdapter<File>(this, android.R.layout.simple_list_item_1, list));
+
+        dirList.setOnItemClickListener( (AdapterView<?> myAdapter, View v, int myItemInt, long mylng) -> {
+
+            File selectedFromList = (File) (dirList.getItemAtPosition(myItemInt));
+            Log.d("selected From File", selectedFromList.getAbsolutePath());
+
+        });
     }
 }
