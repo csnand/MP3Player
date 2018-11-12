7693625699ba54a822e9e05927e14e2525bbdc50
csnand <askmryang@outlook.com>
2018-11-12 12:47:09 +0000

    bk

--- app/src/main/java/com/g53mdp/mp3player/MainActivity.java
+++ app/src/main/java/com/g53mdp/mp3player/MainActivity.java
@@ -1,6 +1,12 @@
 package com.g53mdp.mp3player;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Environment;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,32 +17,67 @@ import android.widget.ListView;
 import java.io.File;
 
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String CHANNEL_ID = "100";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         init();
+        testNotification();
+    }
+
+    private void testNotification(){
+        NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "channel name";
+            String desc = "channel description";
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+
+            channel.setDescription(desc);
+
+            nManager.createNotificationChannel(channel);
+
+
+            int NOTIFICATION_ID = 1;
+
+            Intent nIntent = new Intent();
+
+            nIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pIntent = PendingIntent.getActivity(this, 0, nIntent, 0);
+
+            NotificationCompat.Builder mBuidler = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("notification title")
+                    .setContentText("notificatio text")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            nManager.notify(NOTIFICATION_ID, mBuidler.build());
+        }
+
+
+
     }
 
 
     private void init(){
-
         ListView dirList = (ListView) findViewById(R.id.dirListView);
-
-        File musicDir = new File(Environment.getExternalStorageDirectory().getPath() + "/Music/");
-
-        File list[] = musicDir.listFiles();
+        File musidDir = new File(Environment.getExternalStorageDirectory().getPath() + "/Music/");
+        if (musidDir == null) {
+            musidDir = new File(Environment.getDataDirectory().getPath() + "/Music/");
+        }
+        File list[] = musidDir.listFiles();
 
         dirList.setAdapter(new ArrayAdapter<File>(this, android.R.layout.simple_list_item_1, list));
-
-        dirList.setOnItemClickListener( (AdapterView<?> myAdapter, View v, int myItemInt, long mylng) -> {
-
-            File selectedFromList = (File) (dirList.getItemAtPosition(myItemInt));
-            Log.d("selected From File", selectedFromList.getAbsolutePath());
-
-        });
+        dirList.setOnItemClickListener( (AdapterView<?> parent, View view, int position, long id) -> {
+                File selectedFile = (File) (dirList.getItemAtPosition(position));
+                Log.d("selected", selectedFile.getAbsolutePath() );
+            } );
     }
 }
